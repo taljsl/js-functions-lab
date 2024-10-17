60f55604c209cea0f6cbdd23cea6f76f336f85e7
Josh L <joshua.levine42@gmail.com>
2024-10-17 19:05:50 -0400

    first 3 functions created

---
+++ app.js
@@ -0,0 +1,63 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+const isAdult = number => {
+    if(number >= 18) {
+        return 'Adult';
+    } else {
+        return'Minor';
+    }
+}
+
+
+console.log('Exercise 2 Result:', isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = char =>  {
+    const vowel = ['a','A','e','E','i','I','o','O','u','U']
+    if(vowel.includes(char)){ //learned about includes from chatgpt
+        return true;          // originally was comparing char to vowel[0...]
+    }else{
+        return false;
+    }
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
